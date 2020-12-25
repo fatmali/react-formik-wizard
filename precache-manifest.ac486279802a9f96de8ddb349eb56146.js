9bb3ce99416ab6ca3d34beb70e89a071e783d964
Fatma Ali <alifatma2019@gmail.com>
2020-12-25 21:01:48 +0300

    Updates

---
+++ precache-manifest.ac486279802a9f96de8ddb349eb56146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c695df9386f17750e1b0938308c557a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f98b19f24def0f657b4e",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "0638282ae0a2c488a57f",
+    "url": "./static/js/2.23bc3e41.chunk.js"
+  },
+  {
+    "revision": "6d11e451387012a4ed7c8e91c72c8dde",
+    "url": "./static/js/2.23bc3e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f98b19f24def0f657b4e",
+    "url": "./static/js/main.4c91335c.chunk.js"
+  },
+  {
+    "revision": "2df8f1f31bc4d8ea8e18",
+    "url": "./static/js/runtime-main.5cfffc06.js"
+  }
+]);
